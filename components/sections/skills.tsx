c1bfc6c533338361320d36c92daceab95ed2e0a2
Isadora Paz <mibp@ic.ufal.br>
2026-02-03 21:10:21 -0300

    :sparkles: feat: Skills section

--- components/sections/skills.tsx
+++ components/sections/skills.tsx
@@ -1,6 +1,63 @@
 "use client";
+
+import { Container } from "@/components/misc/container";
+import { SectionTitle } from "@/components/misc/section-title";
+import { childVariants } from "@/config/motion";
+import { skills } from "@/config/skills";
+import { IconSwords } from "@tabler/icons-react";
+import { motion } from "framer-motion";
+import ProfileIllustration from "../illustrations/profile";
+import { SkillsGrid } from "../misc/skills-grid";
+
 export const Skills = () => {
   return (
-    <div>Skills</div>
+    <section id="skills" className="flex flex-col gap-12 w-full">
+      <SectionTitle title="Skillset" description="What we're fighting with" icon={IconSwords} />
+      <div className="flex w-full items-center justify-between gap-24">
+        <div className="flex flex-col gap-8 grow">
+          <Container title="Frameworks">
+            <SkillsGrid>
+              {skills.frameworks.map((framework) => (
+                <motion.div key={framework.name} variants={childVariants} className="flex flex-row items-center gap-2">
+                  <framework.icon size={24} />
+                  <p>{framework.name}</p>
+                </motion.div>
+              ))}
+            </SkillsGrid>
+          </Container>
+          <Container title="Tools and Technologies">
+            <SkillsGrid>
+              {skills.tools.map((tool) => (
+                <motion.div key={tool.name} variants={childVariants} className="flex flex-row items-center gap-2">
+                  <tool.icon size={24} />
+                  <p>{tool.name}</p>
+                </motion.div>
+              ))}
+            </SkillsGrid>
+          </Container>
+          <Container title="Languages">
+            <SkillsGrid>
+              {skills.languages.map((language) => (
+                <motion.div key={language.name} variants={childVariants} className="flex flex-row items-center gap-2">
+                  <language.icon size={24} />
+                  <p>{language.name}</p>
+                </motion.div>
+              ))}
+            </SkillsGrid>
+          </Container>
+          <Container title="Other stuff in my bag">
+            <SkillsGrid>
+              {skills.misc.map((misc) => (
+                <motion.div key={misc.name} variants={childVariants} className="flex flex-row items-center gap-2">
+                  <misc.icon size={24} />
+                  <p>{misc.name}</p>
+                </motion.div>
+              ))}
+            </SkillsGrid>
+          </Container>
+        </div>
+        <ProfileIllustration className="w-lg" />
+      </div>
+    </section>
   )
 }
